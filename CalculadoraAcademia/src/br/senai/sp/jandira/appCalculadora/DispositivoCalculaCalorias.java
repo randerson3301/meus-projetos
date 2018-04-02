d53eb65452a31b095e092cb398e37322947a6a2f
Mendes <Mendes@DESKTOP-EQS2K4F>
2018-04-02 18:38:45 -0300

    Primeira sincronização.

---
+++ CalculadoraAcademia/src/br/senai/sp/jandira/appCalculadora/DispositivoCalculaCalorias.java
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.appCalculadora;
+
+import br.senai.sp.jandira.frameCalculadora.FrmCalculaCalorias;
+
+public class DispositivoCalculaCalorias {
+	public static void main(String[] args) {
+		//Instanciando objeto
+		FrmCalculaCalorias calculadoraAcademia = new FrmCalculaCalorias();
+	
+	
+		
+	}
+}
+
